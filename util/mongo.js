43dabf836a895a9c97dfe8d1cce703354fe11735
Kristina Mouly <kristina.dobrotic@gmail.com>
2022-11-19 07:40:28 +0100

    change

--- util/mongo.js
+++ util/mongo.js
@@ -4,15 +4,11 @@ const MONGO_URL = process.env.MONGO_URL
 
 if (!MONGO_URL) {
   throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
+    
   )
 }
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
+
 let cached = global.mongoose
 
 if (!cached) {
